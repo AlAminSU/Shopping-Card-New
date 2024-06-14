122dfafd84237cbec3023c9239290a7996568a4f
Al Amin <mdalamin02536@gmai.com>
2024-06-14 22:10:53 +0600

    Added storae file

---
+++ storage.js
@@ -0,0 +1,8 @@
+// const userObj = {
+//     name: 'alamin'
+// }
+
+// localStorage.setItem('user', JSON.stringify(userObj))
+// const getStore = localStorage.getItem('user')
+// const getData = JSON.parse(getStore)
+// console.log(getData)
